4efbdcd7256e99387e1b554ce2cee0fadc878f7e
MasanoriIkeda <107474470+yuuzaki2000@users.noreply.github.com>
2022-06-26 20:20:14 +0900

    課題提出

---
+++ src/FizzBuzz.java
@@ -0,0 +1,20 @@
+
+public class FizzBuzz {
+
+	public static void main(String[] args) {
+		for(int count =1;count<=100;count++) {
+			if(count%3 ==0 && count%5 ==0) {
+				System.out.println("FizzBuzz");
+			}else if(count%3 == 0) {
+				System.out.println("Fizz");
+				
+			}else if(count%5 == 0) {
+				System.out.println("Buzz");
+			}else {
+				System.out.println(count);
+			}
+		}
+
+	}
+
+}
